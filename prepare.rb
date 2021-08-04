c43cb99ce6c71990ce95ebacbb7b3086ffd3afc8
wuzhiying <1356424371@qq.com>
2021-08-04 09:31:52 +0800

    完成打包脚本代码写入部分

--- prepare.rb
+++ prepare.rb
@@ -6,7 +6,7 @@ class Xcode
   end
 
   def process
-    project = Xcodeproj::Project.open("/Users/wuzhiying/PrepareLipo/AutoPack/Example/AutoPack.xcodeproj")
+    project = Xcodeproj::Project.open("/Users/wuwenqiu/PrepareLipo/AutoPack/Example/AutoPack.xcodeproj")
     project_name = File.basename(project.path,".xcodeproj")
     process_binary_target(project, project_name)
     process_aggregate_target(project, project_name)
@@ -47,7 +47,9 @@ class Xcode
     # 设置Build Phase,添加Run Script
     aggregate_target.new_shell_script_build_phase("Run build.sh Script")
     # 初始化build.sh
-    write_shell(project, project_name + "Binary")
+    if write_shell(project, project_name + "Binary")
+      puts "初始化build.sh打包脚本✅"
+    end
     # 设置shell_script内容
     aggregate_target.shell_script_build_phases.first.shell_script = "sh ../build.sh"
   end
@@ -78,28 +80,82 @@ class Xcode
   end
 
   # 初始化打包shell脚本内容
-  def write_shell(project, target_name)
+  def write_shell(project, binary_target_name)
     # 打开组件主目录
     dir_name = project.project_dir.parent
     # 新建shell脚本
-    puts dir_name.join("build.sh")
-    build_sh = File.new(dir_name.join("build.sh"),"w+")
-    # 写入打包代码
-    if build_sh
-      build_sh.syswrite("")
-    else
-      puts "新建/打开build.sh失败"
+    build_sh = File.open(dir_name.join("build.sh"),"w+") do |file|
+    file.puts <<EOF # 利用块写入打包代码
+set -e
+set +u
+### Avoid recursively calling this script.
+if [[ $UF_MASTER_SCRIPT_RUNNING ]]
+then
+exit 0
+fi
+set -u
+export UF_MASTER_SCRIPT_RUNNING=1
+### Constants.
+# 静态库target对应的scheme名称
+SCHEMENAME="#{binary_target_name}"
+# .a与头文件生成的目录，在项目中的HBAuthenticationBinary目录下的Products目录中
+BASEBUILDDIR=$PWD/${SCHEMENAME}/Products
+rm -fr "${BASEBUILDDIR}"
+mkdir "${BASEBUILDDIR}"
+# 支持全架构的二进制文件目录
+UNIVERSAL_OUTPUTFOLDER=${BASEBUILDDIR}/Binary-universal
+# 支持真机的二进制文件目录
+IPHONE_DEVICE_BUILD_DIR=${BASEBUILDDIR}/Binary-iphoneos
+# 支持模拟器的二进制文件目录
+IPHONE_SIMULATOR_BUILD_DIR=${BASEBUILDDIR}/Binary-iphonesimulator
+### Functions
+## List files in the specified directory, storing to the specified array.
+#
+# @param $1 The path to list
+# @param $2 The name of the array to fill
+#
+##
+list_files ()
+{
+    filelist=$(ls "$1")
+    while read line
+    do
+        eval "$2[\${\#$2[*]}]=\"\$line\""
+    done <<< "$filelist"
+}
+### Take build target.
+if [[ "$SDK_NAME" =~ ([A-Za-z]+) ]]
+then
+SF_SDK_PLATFORM=${BASH_REMATCH[1]} # "iphoneos" or "iphonesimulator".
+else
+echo "Could not find platform name from SDK_NAME: $SDK_NAME"
+exit 1
+fi
+echo "===== 构建x86_64架构 ====="
+xcodebuild -workspace "${PROJECT_NAME}.xcworkspace" -scheme "${SCHEMENAME}" -configuration "${CONFIGURATION}" -sdk iphonesimulator CONFIGURATION_BUILD_DIR="${IPHONE_SIMULATOR_BUILD_DIR}/x86_64" OBJROOT="${OBJROOT}/DependantBuilds" ARCHS='x86_64' VALID_ARCHS='x86_64' $ACTION
+# Build device platform. (armv7, arm64)
+echo "========== Build Device Platform =========="
+echo "===== Build Device Platform: armv7 ====="
+xcodebuild -workspace "${PROJECT_NAME}.xcworkspace" -scheme "${SCHEMENAME}" -configuration "${CONFIGURATION}" -sdk iphoneos CONFIGURATION_BUILD_DIR="${IPHONE_DEVICE_BUILD_DIR}/armv7" ARCHS='armv7 armv7s' VALID_ARCHS='armv7 armv7s' OBJROOT="${OBJROOT}/DependantBuilds" $ACTION
+echo "===== Build Device Platform: arm64 ====="
+xcodebuild -workspace "${PROJECT_NAME}.xcworkspace" -scheme "${SCHEMENAME}" -configuration "${CONFIGURATION}" -sdk iphoneos CONFIGURATION_BUILD_DIR="${IPHONE_DEVICE_BUILD_DIR}/arm64" ARCHS='arm64' VALID_ARCHS='arm64' OBJROOT="${OBJROOT}/DependantBuilds" $ACTION
+### Build universal platform.
+echo "========== Build Universal Platform =========="
+## Copy the framework structure to the universal folder (clean it first).
+rm -rf "${UNIVERSAL_OUTPUTFOLDER}"
+mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
+## Copy the last product files of xcodebuild command.
+cp -R "${IPHONE_DEVICE_BUILD_DIR}/arm64/lib${SCHEMENAME}.a" "${UNIVERSAL_OUTPUTFOLDER}/lib${SCHEMENAME}.a"
+### Smash them together to combine all architectures.
+lipo -create "${IPHONE_SIMULATOR_BUILD_DIR}/x86_64/lib${SCHEMENAME}.a" "${IPHONE_DEVICE_BUILD_DIR}/armv7/lib${SCHEMENAME}.a" "${IPHONE_DEVICE_BUILD_DIR}/arm64/lib${SCHEMENAME}.a" -output "${UNIVERSAL_OUTPUTFOLDER}/lib${SCHEMENAME}.a"
+
+echo "========== Create Standard Structure =========="
+cp -r "${IPHONE_DEVICE_BUILD_DIR}/arm64/usr/local/include/" "${UNIVERSAL_OUTPUTFOLDER}/include/"
+EOF
     end
+    1
   end
 
-end
-
-class Shell
-  def Shell.content
-    content = %/
-
-    /
-  end
 end
 
 Xcode.new().process
